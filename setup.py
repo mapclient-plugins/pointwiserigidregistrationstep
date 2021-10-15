4f5258c135748557097becb4e8614a96779da2d0
TimmyNic <tim.nicolas@outlook.com>
2021-10-15 13:13:40 +1300

    Updated the way dependencies are handled.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup, find_packages
-import sys, os, io
+import io
 
-# List all of your Python package dependencies in the
-# requirements.txt file
 
 def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
@@ -10,26 +8,33 @@ def readfile(filename, split=False):
             return stream.read().split("\n")
         return stream.read()
 
-readme = readfile("README.md", split=True)[3:]  # skip title
-requires = readfile("requirements.txt", split=True)
-license = readfile("LICENSE")
+
+package_readme = readfile("README.md", split=True)[3:]  # skip title
+package_license = readfile("LICENSE")
+package_dependencies = [
+    "setuptools",
+    "PySide2",
+    "numpy",
+    "gias2",
+    "traits"
+]
 
 setup(name=u'mapclientplugins.pointwiserigidregistrationstep',
-    version='1.0.1',
-    description='',
-    long_description='\n'.join(readme) + license,
-    classifiers=[
-      "Development Status :: 4 - Beta",
-      "License :: OSI Approved :: Apache Software License",
-      "Programming Language :: Python",
-    ],
-    author=u'Ju Zhang',
-    author_email='',
-    url='https://github.com/mapclient-plugins/pointwiserigidregistrationstep',
-    license='APACHE',
-    packages=find_packages(exclude=['ez_setup',]),
-    namespace_packages=['mapclientplugins'],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=requires,
-    )
+      version='1.0.1',
+      description='',
+      long_description='\n'.join(package_readme) + package_license,
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+      ],
+      author=u'Ju Zhang',
+      author_email='',
+      url='https://github.com/mapclient-plugins/pointwiserigidregistrationstep',
+      license='APACHE',
+      packages=find_packages(exclude=['ez_setup', ]),
+      namespace_packages=['mapclientplugins'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=package_dependencies,
+      )
